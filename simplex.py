506a01b3cfd0f5cb929e6ce1593beb06025ced30
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2017-09-13 14:56:01 -0300

    fixed syntax errors

--- simplex.py
+++ simplex.py
@@ -1,19 +1,27 @@
-def print_tab(t):
+def string_tab(t):
     """Prints the tableau t."""
-    print("-"*len(t[0])*11)
+    a = ""
+    a.join("-"*len(t[0])*11)
+    a.join("\n")
     for l in t:
         for e in l[:-1]:
-            print("%10f " % e)
-        print("| ")
-        print("%f" % l[-1])
+            a.join("%10f " % e)
+        a.join("| ")
+        a.join("%f" % l[-1])
+        a.join("\n")
+    a.join("\n")
+    return a
 
 
-def print_sol(s):
+def string_sol(s):
     """Prints the solution s."""
     i = 0
+    a = ""
     for e in s:
         i += 1
-        print("x%d = %f" % i, e)
+        a.join("x%d = %f " % i, e)
+    a.join("\n")
+    return a
 
 
 class Simplex(object):
@@ -22,7 +30,6 @@ class Simplex(object):
     def __init__(self, F, A, B):
         """F is the objective funcion, A is the restriction functions
         and B is the value of the restriction functions"""
-        i = 0
         self.iter = 0 # initial iteration for the functions next, prev
         self.f = F    # objective function
         self.a = A    # LHS of restriction equations
@@ -31,24 +38,32 @@ class Simplex(object):
         self.t_hist = [] # history of tableaus of each iteration
         self.s_hist = [] # hisory of solutions for each iteration
         self.sol = None  # solution of the problem
-        self.offset = 0  # offset for where ends phase1 and begins pase2
+        self.offset = 0  # offset for where end phase1 and begin pase2
 
+        self.m = [[0]*(len(A[0])+1) for i in range(len(A)+1)]
+
+        # creating tableau M 
         for i in range(len(A)+1):
-            self.m[0][i] = [F[i] if len(F) > i else 0]
-            self.m[i] = [e for e in [B[i] if (i == len(A)) else A[i]]]
+            self.m[0][i] = F[i] if len(F) > i else 0
+            if i != 0:
+                self.m[i] = [e for e in A[i-1]+[B[i-1]]]
 
 
     def __str__(self):
         """Prints the tableau of each iteration and the solution."""
-    i = 0
-    for t, s in zip(self.t_hist, self.s_hist):
-        print("Iteration %2d" % i)
-        print("============\n")
-        print_tab(t)
-        print("\n")
-        print_sol(s)
-        print("\n")
-        i += 1
+        i = 0
+        th = self.t_hist
+        sh = self.s_hist
+        s = ""
+        for t, s in zip(th, sh):
+            s.join("Iteration %2d\n" % i)
+            s.join("============\n\n")
+            string_tab(t)
+            s.join("\n\n")
+            string_sol(s)
+            s.join("\n\n")
+            i += 1
+        return s
 
 
     def phase1(self, verbose):
@@ -62,40 +77,49 @@ class Simplex(object):
 
     def phase2(self, init, verbose):
         """Second phase of the simplex algorithm."""
-        while (any(n < 0 for n in M[:-1])):
+        M = self.m
+        while (any(n < 0 for n in M[0][:-1])):
             if verbose:
                 print("Phase 2:\n")
             self.iter += 1
 
             # min coefficient enters base
 
-            min_coef = 0 # minimum coefficient in the objective function
-            enter = None # index in the list self.sol of the variable with
-                         # minimum coefficient
+            min_coef = 0 # minimum coefficient in the objective func
+            enter = None # index in the list self.sol of the variable
+                         # with minimum coefficient
 
-            for i in len(M[0][:-1]):
-                if (e = M[0][i] < min_coef):
+            for i in range(len(M[0][:-1])):
+                if (M[0][i] < min_coef):
                     enter = i
-                    min_coef = e
+                    min_coef = M[0][i]
+
+            if verbose:
+                print("Enter: x%d" % enter)
 
             # min value drops base
 
             min_val = 0  # minimum value in RHS/(enter colum)
-            drop = None  # index in the list self.sol of the variable with
-                         # minimum positive value
+            drop = None  # index in the list self.sol of the variable
+                         # with minimum positive value
 
-            for i in len(M[:-1]):
-                if (M[i][enter] != 0 and e = M[i][-1]/M[i][enter] and a > 0):
+            for i in range(len(M[:-1])):
+                if (M[i][enter] != 0 and M[i][-1]/float(M[i][enter])\
+                        and M[i][-1]/float(M[i][enter]) > 0):
                     drop = i
-                    min_val = e
+                    min_val = M[i][-1]/float(M[i][enter])
+
+            if verbose:
+                print("Leave: x%d" % drop)
 
             # escalonate matrix M
 
-            [M[drop][i] = M[drop][i]/M[drop][enter] for i in range(len(M))] 
-
+            M[drop] = [M[drop][i]/M[drop][enter] for i in range(len(M))] 
             for l in M:
                 if l != M[drop]:
-                    [l[i] = l[i] - M[drop][i] for i in range(len(M))]
+                    l = [l[i] - M[drop][i] for i in range(len(M))]
+
+            print(string_tab(M))
 
 
     def solve(self, verbose = False):
