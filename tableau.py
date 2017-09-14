1b61e694c06b0e665e43ddfd540ab6949878b0e5
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2017-09-14 18:24:26 -0300

    implemented multiplying methods

--- tableau.py
+++ tableau.py
@@ -1,6 +1,6 @@
 class Tableau(object):
     def __init__(self, F, A, B):
-        """ F is a list with (self.vars_num + 1) elements.
+        """F is a list with (self.vars_num + 1) elements.
         A is an array with self.const_num elements and each
         element of A has self.vars_num elements.
         B is a list with self.const_num elements."""
@@ -22,22 +22,6 @@ class Tableau(object):
         self.lines   = self.const_nums+1
         self.m = self.create_tab()
 
-    def create_tab(self):
-        A = self.const_func
-        B = self.const_vals
-        # initialize the tableau lines
-        m = [0]*self.lines
-
-        # first line is the objective function
-        m[0] = [self.obj_func[i] for i in range(self.columns-1)]
-        m[0].append(0)
-
-        # other lines are constraints
-        for i in range(1,self.lines):
-            m[i] = [ A[i-1][j] for j in range(self.columns-1)]
-            m[i].append(B[i-1])
-
-        return m
 
     def __str__(self):
         """Print the function being minimized/maximized, a dashed line
@@ -53,4 +37,36 @@ class Tableau(object):
                 s += (" %8.2f |" % e)
             s += ("\n")
         return s
+
+
+    def create_tab(self):
+        """Create the tableau array with self.lines x self.columns"""
+        A = self.const_func
+        B = self.const_vals
+
+        # initialize the tableau lines
+        m = [0]*self.lines
+
+        # first line is the objective function
+        m[0] = [self.obj_func[i] for i in range(self.columns-1)]
+        m[0].append(0)
+
+        # other lines are constraints of base variables
+        for i in range(1,self.lines):
+            m[i] = [ A[i-1][j] for j in range(self.columns-1)]
+            m[i].append(B[i-1])
+
+        return m
+
+
+    def mult_line_by(self, lin, num):
+        """Multiply line lin by num."""
+        for i in range(self.columns):
+            self.m[lin][i] *= num
+
+
+    def mult_column_by(self, col, num):
+        """Multiply column col by num."""
+        for j in range(self.lines):
+            self.m[j][col] *= num
 
